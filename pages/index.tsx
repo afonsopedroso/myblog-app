7377a7fd1dfbea8e70e02b126aba5c94329e9fd0
Afonso Pedroso <78538003+afonsopedroso@users.noreply.github.com>
2025-02-13 12:41:25 +0000

    fix: all

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,12 +68,13 @@ export default function Home({posts}:Post) {
                 )
               } 
               className={`px-1 h-7 flex items-center justify-center py-4 m-[2px] border rounded ${
-              selectedTag.includes(tag) ? "bg-green-500 text-white" : "bg-black"
-            }`}>{tag}</button>
+              selectedTag.includes(tag) ? "bg-green-500 text-white" : selectedTag.length >= 5 
+              ? 'bg-slate-500' : 'bg-black'
+            }`} >{tag}</button>
             ))}
             {selectedTag.length!=0 && (
           <button onClick={() => setSelectedTag([])} className="ml-[2px] mt-[2px] bg-red-200 rounded-md h-8 text-red-500">
-            Clear
+            <div className="flex justify-center items-center"><span>Clear</span>  <img className="w-8 h-6 pl-1" src="/clear.png" alt="" /></div>     
           </button>
         )}
           </div>
